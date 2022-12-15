e8709194a563ab4372cd8f456f71dfeb10087f6b
Luis Fernando <fercho.vargas.55.nano@gmail.com>
2022-12-15 18:01:43 -0400

    fixed

--- database/db.js
+++ database/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const dbConexion = async() => {
     try {
+        mongoose.set("strictQuery", false);
         await mongoose.connect( process.env.MONGO_DB);
         console.log('Base de datos iniciada correctamente');
     } catch (error) {
